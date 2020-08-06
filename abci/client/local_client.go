6362a3f1cfaad6126553985fd4ce926ab797ec39
Jiyong Ha <slave5vw@gmail.com>
2020-08-06 13:18:09 +0900

    fix: rollback original localclient

--- abci/client/local_client.go
+++ abci/client/local_client.go
@@ -82,6 +82,9 @@ func (app *localClient) SetOptionAsync(req types.RequestSetOption) *ReqRes {
 }
 
 func (app *localClient) DeliverTxAsync(params types.RequestDeliverTx) *ReqRes {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
+
 	res := app.Application.DeliverTx(params)
 	return app.callback(
 		types.ToRequestDeliverTx(params),
